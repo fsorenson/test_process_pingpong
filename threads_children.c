6ec667d0d3f555f32e79d5243ef066c65bec6b5f
Frank Sorenson <frank@tuxrocks.com>
2013-04-13 23:51:45 -0500

    Fix up the individual ping/pong function calls to actually _use_ them
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- threads_children.c
+++ threads_children.c
@@ -107,9 +107,9 @@ void __attribute__((noreturn)) do_thread_work(int thread_num) {
 
 
 	if (thread_num == 0) {
-		comm_do_ping(thread_num);
+		config.comm_do_ping(thread_num);
 	} else {
-		comm_do_pong(thread_num);
+		config.comm_do_pong(thread_num);
 	}
 
 
