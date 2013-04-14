4b2e45a06f67d2c7cf3cb73fd606db801d2ae5ca
Frank Sorenson <frank@tuxrocks.com>
2013-04-13 21:52:29 -0500

    New ping and pong functions for semaphores (and busysem).
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- sem.c
+++ sem.c
@@ -35,6 +35,51 @@ int make_sem_pair(int fd[2]) {
 	return 0;
 }
 
+int do_begin_sem() {
+	return sem_post(sems[0]);
+}
+
+inline int __attribute__((noreturn)) __attribute__((hot)) __attribute__((optimize("-Ofast"))) do_ping_sem(int thread_num) {
+	(void)thread_num;
+
+	while (1) {
+		run_data->ping_count ++;
+
+		while (sem_wait(sems[0]) != 0);
+		while (sem_post(sems[1]) != 0);
+	}
+}
+
+inline int __attribute__((noreturn)) __attribute__((hot)) __attribute__((optimize("-Ofast"))) do_pong_sem(int thread_num) {
+	(void)thread_num;
+
+	while (1) {
+		while (sem_wait(sems[1]) != 0);
+		while (sem_post(sems[0]) != 0);
+	}
+}
+
+inline int __attribute__((hot)) __attribute__((optimize("-Ofast"))) do_ping_busysem(int thread_num) {
+	(void)thread_num;
+
+	while (1) {
+		run_data->ping_count ++;
+
+		while (sem_trywait(sems[0]) == -1);
+		while (sem_post(sems[1]) != 0);
+	}
+}
+
+inline int __attribute__((hot)) __attribute__((optimize("-Ofast"))) do_pong_busysem(int thread_num) {
+	(void)thread_num;
+
+	while (1) {
+		while (sem_trywait(sems[1]) == -1);
+		while (sem_post(sems[0]) != 0);
+	}
+}
+
+
 inline int do_send_sem(int fd) {
 
         return ! sem_post(sems[fd]);
@@ -69,6 +114,9 @@ void __attribute__((constructor)) comm_add_sem() {
 
 	memset(&ops, 0, sizeof(struct comm_mode_ops_struct));
 	ops.comm_make_pair = make_sem_pair;
+	ops.comm_begin = do_begin_sem;
+	ops.comm_do_ping = do_ping_sem;
+	ops.comm_do_pong = do_pong_sem;
 	ops.comm_do_send = do_send_sem;
 	ops.comm_do_recv = do_recv_sem;
 	ops.comm_cleanup = cleanup_sem;
@@ -82,6 +130,9 @@ void __attribute__((constructor)) comm_add_busy_sem() {
 
 	memset(&ops, 0, sizeof(struct comm_mode_ops_struct));
 	ops.comm_make_pair = make_sem_pair;
+	ops.comm_begin = do_begin_sem;
+	ops.comm_do_ping = do_ping_busysem;
+	ops.comm_do_pong = do_pong_busysem;
 	ops.comm_do_send = do_send_sem;
 	ops.comm_do_recv = do_recv_busy_sem;
 	ops.comm_cleanup = cleanup_sem;
