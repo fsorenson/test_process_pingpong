18440e43bfeef6bc4b5bc2a0530e0f42d0cda7f0
Frank Sorenson <frank@tuxrocks.com>
2013-04-15 23:39:38 -0500

    Use new init struct and function
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/socket_pair.c
+++ comms/socket_pair.c
@@ -10,12 +10,16 @@ int make_socket_pair(int fd[2]) {
 }
 
 void __attribute__((constructor)) comm_add_socket_pair() {
+	struct comm_mode_init_info_struct init_info;
 	struct comm_mode_ops_struct ops;
+
+	memset(&init_info, 0, sizeof(struct comm_mode_init_info_struct));
+	init_info.name = "socketpair";
 
 	memset(&ops, 0, sizeof(struct comm_mode_ops_struct));
 	ops.comm_make_pair = make_socket_pair;
 
-	comm_mode_do_initialization("socketpair", &ops);
+	comm_mode_do_initialization(&init_info, &ops);
 
 }
 
