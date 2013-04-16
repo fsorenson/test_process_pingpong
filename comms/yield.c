cf69a913e44aaa7593a1243e7798ab23758b877c
Frank Sorenson <frank@tuxrocks.com>
2013-04-16 00:10:24 -0500

    New 'yield_nop' comm which effectively does what the old 'yield' did...  thread 0 yields once for each time around, and thread 1 just sleeps.
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/yield.c
+++ comms/yield.c
@@ -11,14 +11,21 @@
 static int volatile *yield_var;
 
 struct timespec yield_ts;
+static bool nop = false;
 
 int make_yield_pair(int fd[2]) {
 	static int yield_num = 0;
 
 	if (yield_num == 0) {
+		if (!strncmp(get_comm_mode_name(config.comm_mode_index), "yield_nop", 9))
+			nop = true;
+
+		if (nop == false) {
 			yield_var = mmap(NULL, sizeof(int),
 				PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 			*yield_var = 0;
+		}
+
 		yield_ts.tv_sec = 0;
 		yield_ts.tv_nsec = 1000000;
 	}
@@ -55,6 +62,17 @@ inline int __PINGPONG_FN do_pong_yield(int thread_num) {
 	}
 }
 
+
+inline int __PINGPONG_FN do_ping_yield_nop(int thread_num) {
+	(void)thread_num;
+
+	while (1) {
+		run_data->ping_count ++;
+		sched_yield();
+	}
+}
+
+inline int __PINGPONG_FN do_pong_yield_nop(int thread_num) {
 	(void)thread_num;
 
 	while (1) {
@@ -93,6 +111,20 @@ void __attribute__((constructor)) comm_add_yield() {
 	ops.comm_do_recv = do_recv_yield;
 
 	comm_mode_do_initialization(&init_info, &ops);
+}
+void __attribute__((constructor)) comm_add_yield_nop() {
+	struct comm_mode_init_info_struct init_info;
+	struct comm_mode_ops_struct ops;
 
+	memset(&init_info, 0, sizeof(struct comm_mode_init_info_struct));
+	init_info.name = "yield_nop";
+
+	memset(&ops, 0, sizeof(struct comm_mode_ops_struct));
+	ops.comm_make_pair = make_yield_pair;
+	ops.comm_do_ping = do_ping_yield_nop;
+	ops.comm_do_pong = do_pong_yield_nop;
+
+	comm_mode_do_initialization(&init_info, &ops);
 }
 ADD_COMM_MODE(yield, comm_add_yield);
+ADD_COMM_MODE(yield_nop, comm_add_yield_nop);
--- comms/yield.h
+++ comms/yield.h
@@ -7,11 +7,14 @@ int make_yield_pair(int fd[2]);
 
 int do_ping_yield(int thread_num);
 int do_pong_yield(int thread_num);
+int do_ping_yield_nop(int thread_num);
+int do_pong_yield_nop(int thread_num);
 
 int do_send_yield(int fd);
 int do_recv_yield(int fd);
 
 
 void comm_add_yield();
+void comm_add_yield_nop();
 
 #endif
