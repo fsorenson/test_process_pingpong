7cc3e452161650678ba375082ed0c8aeaaca675a
Frank Sorenson <frank@tuxrocks.com>
2013-04-16 22:05:06 -0500

    Header files fixup
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/inotify.c
+++ comms/inotify.c
@@ -4,7 +4,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <linux/inotify.h>
+#include <sys/inotify.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include <errno.h>
 
 
 
