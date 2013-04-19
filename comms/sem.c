51f39af9ca5a6e244d112c4bdf5ddce67a9ca1c0
Frank Sorenson <frank@tuxrocks.com>
2013-04-19 05:23:50 -0500

    Deprecate old send/recv functions in favor of ping/pong functions.
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/sem.c
+++ comms/sem.c
@@ -79,27 +79,6 @@ inline int __PINGPONG_FN do_pong_busysem(int thread_num) {
 	}
 }
 
-
-inline int do_send_sem(int fd) {
-
-        return ! sem_post(sems[fd]);
-}
-inline int do_recv_sem(int fd) {
-        int err;
-
-	err = sem_wait(sems[fd]);
-        return ! err;
-}
-inline int do_recv_busy_sem(int fd) {
-        int err;
-
-       do {
-		if ((err = sem_trywait(sems[fd])) == -1)
-			err = errno;
-	} while (err == EAGAIN);
-
-        return ! err;
-}
 int cleanup_sem() {
 	sem_close(sems[0]);
 	sem_unlink(sem_names[0]);
@@ -122,8 +101,6 @@ void __attribute__((constructor)) comm_add_sem() {
 	ops.comm_begin = do_begin_sem;
 	ops.comm_do_ping = do_ping_sem;
 	ops.comm_do_pong = do_pong_sem;
-	ops.comm_do_send = do_send_sem;
-	ops.comm_do_recv = do_recv_sem;
 	ops.comm_cleanup = cleanup_sem;
 
 
@@ -143,8 +120,6 @@ void __attribute__((constructor)) comm_add_busy_sem() {
 	ops.comm_begin = do_begin_sem;
 	ops.comm_do_ping = do_ping_busysem;
 	ops.comm_do_pong = do_pong_busysem;
-	ops.comm_do_send = do_send_sem;
-	ops.comm_do_recv = do_recv_busy_sem;
 	ops.comm_cleanup = cleanup_sem;
 
 
