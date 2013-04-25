2933a19544e13f6350fdfc88f0d90f0507202b7d
Frank Sorenson <frank@tuxrocks.com>
2013-04-25 09:22:11 -0500

    Cleanup prototypes for comms/sem.h
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/sem.h
+++ comms/sem.h
@@ -9,7 +9,7 @@
 
 
 int make_sem_pair(int fd[2]);
-int do_begin_sem();
+int do_begin_sem(void);
 
 int do_ping_sem(int thread_num);
 int do_pong_sem(int thread_num);
@@ -20,9 +20,9 @@ int do_send_sem(int fd);
 int do_recv_sem(int fd);
 int do_recv_busy_sem(int fd);
 
-int cleanup_sem();
+int cleanup_sem(void);
 
-void comm_add_sem();
-void comm_add_busy_sem();
+void comm_add_sem(void);
+void comm_add_busy_sem(void);
 
 #endif
