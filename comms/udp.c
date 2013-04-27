48cf52615549da2fe3f2d10e142615a6f9843db1
Frank Sorenson <frank@tuxrocks.com>
2013-04-26 22:01:14 -0500

    Change constructor functions for comms/udp.c
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/udp.c
+++ comms/udp.c
@@ -7,6 +7,8 @@
 #include <netinet/ip.h>
 #include <sys/socket.h>
 
+char comm_name_udp[] = "udp";
+
 int make_udp_pair(int fd[2]) {
         int fds, fdc;
         socklen_t addr_len;
@@ -47,18 +49,16 @@ int make_udp_pair(int fd[2]) {
         return 0;
 }
 
+static struct comm_mode_init_info_struct comm_info_udp = {
+	.name = comm_name_udp
+};
+
+static struct comm_mode_ops_struct comm_ops_udp = {
+	.comm_make_pair = make_udp_pair
+};
+
 void __attribute__((constructor)) comm_add_udp() {
-        struct comm_mode_init_info_struct init_info;
-	struct comm_mode_ops_struct ops;
-
-	memset(&init_info, 0, sizeof(struct comm_mode_init_info_struct));
-	init_info.name = strdup("udp");
-
-	memset(&ops, 0, sizeof(struct comm_mode_ops_struct));
-	ops.comm_make_pair = make_udp_pair;
-
-	comm_mode_do_initialization(&init_info, &ops);
-	free(init_info.name);
+	comm_mode_do_initialization(&comm_info_udp, &comm_ops_udp);
 }
 
 ADD_COMM_MODE(udp, comm_add_udp);
