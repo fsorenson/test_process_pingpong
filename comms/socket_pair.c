d33bda2ecf72c9f6e611e8df94bfb925f17b8196
Frank Sorenson <frank@tuxrocks.com>
2013-04-26 22:01:30 -0500

    Change constructor functions for comms/socket_pair.c
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/socket_pair.c
+++ comms/socket_pair.c
@@ -4,23 +4,23 @@
 #include <string.h>
 #include <sys/socket.h>
 
+char comm_name_socketpair[] = "socketpair";
+
 int make_socket_pair(int fd[2]) {
         socketpair(AF_UNIX, SOCK_STREAM, AF_LOCAL, fd);
         return 0;
 }
 
+static struct comm_mode_init_info_struct comm_info_socketpair = {
+	.name = comm_name_socketpair
+};
+
+static struct comm_mode_ops_struct comm_ops_socketpair = {
+	.comm_make_pair = make_socket_pair
+};
+
 void __attribute__((constructor)) comm_add_socket_pair() {
-	struct comm_mode_init_info_struct init_info;
-	struct comm_mode_ops_struct ops;
-
-	memset(&init_info, 0, sizeof(struct comm_mode_init_info_struct));
-	init_info.name = strdup("socketpair");
-
-	memset(&ops, 0, sizeof(struct comm_mode_ops_struct));
-	ops.comm_make_pair = make_socket_pair;
-
-	comm_mode_do_initialization(&init_info, &ops);
-	free(init_info.name);
+	comm_mode_do_initialization(&comm_info_socketpair, &comm_ops_socketpair);
 }
 
 ADD_COMM_MODE(socketpair, comm_add_socket_pair);
