247e6b348beba97d67164e049980e241ff3f89cd
Frank Sorenson <frank@tuxrocks.com>
2013-04-29 13:39:38 -0500

    Add new 'race2' comm where both threads set their value AND increment the counter.  Not sure what this proves, but it's interesting nonetheless.
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/race.c
+++ comms/race.c
@@ -7,6 +7,8 @@
 
 char comm_name_race1[] = "race1";
 char comm_help_text_race1[] = "both threads repeatedly write their own value--no pingpong";
+char comm_name_race2[] = "race2";
+char comm_help_text_race2[] = "both threads repeatedly write their own value _AND_ increment the counter--no pingpong";
 
 int volatile *race_var;
 
@@ -37,9 +39,28 @@ inline int __PINGPONG_FN do_ping_race1(int thread_num) {
 	}
 }
 
+inline int __PINGPONG_FN do_ping_race2(int thread_num) {
+	(void)thread_num;
+
+	while (1) {
+		run_data->ping_count ++;
+
+		*race_var = 1;
+	}
+}
+
 inline int __PINGPONG_FN do_pong_race1(int thread_num) {
 	(void)thread_num;
 	while (1) {
+		*race_var = 0;
+	}
+}
+
+inline int __PINGPONG_FN do_pong_race2(int thread_num) {
+	(void)thread_num;
+	while (1) {
+		run_data->ping_count ++;
+
 		*race_var = 0;
 	}
 }
@@ -60,8 +81,23 @@ static struct comm_mode_ops_struct comm_ops_race1 = {
 	.comm_do_pong = do_pong_race1,
 	.comm_cleanup = cleanup_race
 };
+static struct comm_mode_init_info_struct comm_info_race2 = {
+	.name = comm_name_race2,
+	.help_text = comm_help_text_race2
+};
+
+static struct comm_mode_ops_struct comm_ops_race2 = {
+	.comm_make_pair = make_race_pair,
+	.comm_do_ping = do_ping_race2,
+	.comm_do_pong = do_pong_race2,
+	.comm_cleanup = cleanup_race
+};
 
 void __attribute__((constructor)) comm_add_race1() {
 	comm_mode_do_initialization(&comm_info_race1, &comm_ops_race1);
 }
+void __attribute__((constructor)) comm_add_race2() {
+	comm_mode_do_initialization(&comm_info_race2, &comm_ops_race2);
+}
+ADD_COMM_MODE(race, comm_add_race2);
 ADD_COMM_MODE(race, comm_add_race1);
--- comms/race.h
+++ comms/race.h
@@ -6,10 +6,13 @@
 int make_race_pair(int fd[2]);
 
 int do_ping_race1(int thread_num);
+int do_ping_race2(int thread_num);
 int do_pong_race1(int thread_num);
+int do_pong_race2(int thread_num);
 
 int cleanup_race(void);
 
 void comm_add_race1(void);
+void comm_add_race2(void);
 
 #endif
