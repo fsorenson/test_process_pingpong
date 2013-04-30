907a13f84e0fed4df58cbff0dc131afe0fef063d
Frank Sorenson <frank@tuxrocks.com>
2013-04-29 23:18:10 -0500

    Silence old-style function definition warnings for comms/nop.c
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/nop.c
+++ comms/nop.c
@@ -97,7 +97,7 @@ inline int __PINGPONG_FN do_pong_nop(int thread_num) {
 		nanosleep(&nop_ts, NULL);
 	}
 }
-int __CONST cleanup_nop() {
+int __CONST cleanup_nop(void) {
 
 	return 0;
 }
@@ -150,16 +150,16 @@ static struct comm_mode_ops_struct comm_ops_nop4 = {
 	.comm_cleanup = cleanup_nop
 };
 
-void __attribute__((constructor)) comm_add_nop1() {
+void __attribute__((constructor)) comm_add_nop1(void) {
 	comm_mode_do_initialization(&comm_info_nop1, &comm_ops_nop1);
 }
-void __attribute__((constructor)) comm_add_nop2() {
+void __attribute__((constructor)) comm_add_nop2(void) {
 	comm_mode_do_initialization(&comm_info_nop2, &comm_ops_nop2);
 }
-void __attribute__((constructor)) comm_add_nop3() {
+void __attribute__((constructor)) comm_add_nop3(void) {
 	comm_mode_do_initialization(&comm_info_nop3, &comm_ops_nop3);
 }
-void __attribute__((constructor)) comm_add_nop4() {
+void __attribute__((constructor)) comm_add_nop4(void) {
 	comm_mode_do_initialization(&comm_info_nop4, &comm_ops_nop4);
 }
 
