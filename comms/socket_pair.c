88116e46eefe183a9ead2559a1b231af4633ce8d
Frank Sorenson <frank@tuxrocks.com>
2013-05-02 09:05:10 -0500

    Remove old initialization method for comms/socket_pair.[ch]
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/socket_pair.c
+++ comms/socket_pair.c
@@ -4,23 +4,14 @@
 #include <string.h>
 #include <sys/socket.h>
 
-char comm_name_socketpair[] = "socketpair";
 
 int make_socket_pair(int fd[2]) {
         socketpair(AF_UNIX, SOCK_STREAM, AF_LOCAL, fd);
         return 0;
 }
 
-static struct comm_mode_init_info_struct comm_info_socketpair = {
-	.name = comm_name_socketpair
-};
-
 static struct comm_mode_ops_struct comm_ops_socketpair = {
 	.comm_make_pair = make_socket_pair
 };
-
-void comm_add_socket_pair(void) {
-	comm_mode_do_initialization(&comm_info_socketpair, &comm_ops_socketpair);
-}
 
 NEW_ADD_COMM_MODE(socketpair, "", &comm_ops_socketpair);
--- comms/socket_pair.h
+++ comms/socket_pair.h
@@ -4,6 +4,5 @@
 #include "comms.h"
 
 int make_socket_pair(int fd[2]);
-void comm_add_socket_pair(void);
 
 #endif
