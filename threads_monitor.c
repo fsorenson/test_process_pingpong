8ea23c6fc29f92752b5475796ac61dba612307df
Frank Sorenson <frank@tuxrocks.com>
2013-05-10 07:05:33 -0500

    Create new output header for stats.
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- threads_monitor.c
+++ threads_monitor.c
@@ -176,10 +176,34 @@ void show_stats(int signum) {
 	if (run_data->stop == true) /* don't worry about output...  let's just pack up and go home */
 		return;
 
-// header
-snprintf(output_buffer, 255, "%5s %12s %11s\n",
-	"time", "cycles/sec", "cycle time");
-write(1, output_buffer, strlen(output_buffer));
+	// original format
+	// 83.000 s - 64962 iterations -> 15.393 us, ping: 40945.64 cycles, pong: 40945.100 cycles
+
+	// header
+	snprintf(output_buffer, output_buffer_len,
+		"%7s %12s %11s", "", "", "");
+	snprintf(output_buffer + strlen(output_buffer), output_buffer_len - strlen(output_buffer),
+		" %s %s",
+		" ___________ PING ___________ ",
+		" ___________ PONG ___________ "
+		);
+	write(1, output_buffer, strlen(output_buffer));
+	write(1, "\n", 1);
+
+	snprintf(output_buffer, output_buffer_len,
+		"%7s %12s %11s",
+		"time", "cycles/sec", "cycle time");
+
+	// per-thread header
+	snprintf(output_buffer + strlen(output_buffer), output_buffer_len - strlen(output_buffer),
+		" | %13s  %5s  %5s",
+		" vol/inv csw", "user", "sys");
+	snprintf(output_buffer + strlen(output_buffer), output_buffer_len - strlen(output_buffer),
+		" | %13s  %5s  %5s",
+		" vol/inv csw", "user", "sys");
+	write(1, output_buffer, strlen(output_buffer));
+
+	write(1, "\n", 1);
 
 
 	snprintf(output_buffer, 255, "%s %12llu %11s",
