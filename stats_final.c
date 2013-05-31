23c073043ce58a143515d1f850a8d7c280f4a4f0
Frank Sorenson <frank@tuxrocks.com>
2013-05-31 15:26:32 -0500

    Split the sched file info into lines.
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- stats_final.c
+++ stats_final.c
@@ -17,9 +17,19 @@ static int __PURE count_sched_lines(const char *buf) {
 }
 
 static void parse_sched_data(void) {
+	char *sched_tok1, *sched_tok2;
+	char *save_ptr1, *save_ptr2;
 	int sched_lines;
 
 	sched_lines = count_sched_lines(run_data->thread_stats[0].sched_data);
+
+	sched_tok1 = strtok_r(run_data->thread_stats[0].sched_data, "\n", &save_ptr1);
+	sched_tok2 = strtok_r(run_data->thread_stats[1].sched_data, "\n", &save_ptr2);
+
+	while((sched_tok1 != NULL) && (sched_tok2 != NULL)) {
+		sched_tok1 = strtok_r(NULL, "\n", &save_ptr1);
+		sched_tok2 = strtok_r(NULL, "\n", &save_ptr2);
+	}
 }
 
 void output_final_stats(void) {
