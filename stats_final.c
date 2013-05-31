8f17fe25f8a571ec19f0282745405e871476d47c
Frank Sorenson <frank@tuxrocks.com>
2013-05-31 15:23:57 -0500

    Count the number of lines in the captured /proc/pid/sched file.
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- stats_final.c
+++ stats_final.c
@@ -1,5 +1,28 @@
 #include "stats_final.h"
 
+#include <string.h>
+
+static int __PURE count_sched_lines(const char *buf) {
+	int count = 0;
+	int index = 0;
+	char *p;
+
+	while (buf + index != NULL) {
+		count ++;
+		p = strchr(buf + index, '\n');
+		if (p != NULL)
+			index ++;
+	}
+	return count;
+}
+
+static void parse_sched_data(void) {
+	int sched_lines;
+
+	sched_lines = count_sched_lines(run_data->thread_stats[0].sched_data);
+}
+
 void output_final_stats(void) {
 
+	parse_sched_data();
 }
