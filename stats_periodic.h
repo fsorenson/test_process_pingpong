92951e014225d867e1062c6ba074cc3b2269015f
Frank Sorenson <frank@tuxrocks.com>
2013-06-06 21:00:12 -0500

    Move periodic stats struct to newly-created file.
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

---
+++ stats_periodic.h
@@ -0,0 +1,22 @@
+#ifndef __STATS_PERIODIC_H__
+#define __STATS_PERIODIC_H__
+
+#include "test_process_pingpong.h"
+
+struct interval_stats_struct {
+	unsigned long long current_count;
+	unsigned long long interval_count;
+	long double current_time;
+	long double interval_time;
+	long double run_time;
+	struct rusage rusage[2];
+	unsigned long long interval_tsc[2];
+
+	long csw[2]; /* per-thread per-interval csw */
+
+	long double iteration_time; /* time for each iteration */
+	long double cpi[2]; /* cycles-per-iteration per CPU */
+	long double mhz[2]; /* approximate speed of the CPU */
+};
+
+#endif
--- threads_monitor.h
+++ threads_monitor.h
@@ -8,21 +8,6 @@
 #include "units.h"
 #include "sched.h"
 
-struct interval_stats_struct {
-	unsigned long long current_count;
-	unsigned long long interval_count;
-	long double current_time;
-	long double interval_time;
-	long double run_time;
-	struct rusage rusage[2];
-	unsigned long long interval_tsc[2];
-
-	long csw[2]; /* per-thread per-interval csw */
-
-	long double iteration_time; /* time for each iteration */
-	long double cpi[2]; /* cycles-per-iteration per CPU */
-	long double mhz[2]; /* approximate speed of the CPU */
-};
 
 /* thread startup, execution, handlers, etc */
 
