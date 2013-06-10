d226160a2feaa5536f390cc1dc65336a4b03cfba
Frank Sorenson <frank@tuxrocks.com>
2013-06-10 15:57:10 -0500

    Create new common stats header.
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- stats_periodic.h
+++ stats_periodic.h
@@ -3,22 +3,6 @@
 
 #include "test_process_pingpong.h"
 
-struct interval_stats_struct {
-	unsigned long long current_count;
-	unsigned long long interval_count;
-	long double current_time;
-	long double interval_time;
-	long double run_time;
-	struct rusage rusage[2];
-	unsigned long long interval_tsc[2];
-
-	long csw[2]; /* per-thread per-interval csw */
-
-	long double iteration_time; /* time for each iteration */
-	long double cpi[2]; /* cycles-per-iteration per CPU */
-	long double mhz[2]; /* approximate speed of the CPU */
-};
-
 int gather_periodic_stats(struct interval_stats_struct *i_stats);
 void show_periodic_stats(void);
 void show_periodic_stats_header(void);
