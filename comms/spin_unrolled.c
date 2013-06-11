752c99e8b9c0ea4567ad55ac51c67fda1ab1d24c
Frank Sorenson <frank@tuxrocks.com>
2013-06-10 21:22:52 -0500

    Add Copyright and GPL license headers to all .[ch] files, and add GPL file.
    Signed-off-by: Frank Sorenson <frank@tuxrocks.com>

--- comms/spin_unrolled.c
+++ comms/spin_unrolled.c
@@ -1,3 +1,24 @@
+/*
+	This file is part of test_process_pingpong
+
+	Copyright 2013 Frank Sorenson (frank@tuxrocks.com)
+
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include "spin_unrolled.h"
 #include "comms.h"
 #include "test_process_pingpong.h"
